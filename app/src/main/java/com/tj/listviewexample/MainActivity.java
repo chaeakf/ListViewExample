e75fd81db8dd2024634fc9ffca8c6ac8d35185b0
Eunsil-Jeong <daramchu@naver.com>
2019-04-20 13:50:15 +0900

    데이터바인딩 예시

--- app/src/main/java/com/tj/listviewexample/MainActivity.java
+++ app/src/main/java/com/tj/listviewexample/MainActivity.java
@@ -1,17 +1,19 @@
 package com.tj.listviewexample;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.tj.listviewexample.adapters.StoreAdapter;
+import com.tj.listviewexample.databinding.ActivityMainBinding;
 import com.tj.listviewexample.datas.Store;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding act;
     List<Store> myStoreList = new ArrayList<>();
     private android.widget.ListView storeListView;
 
@@ -20,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
         bindVies();
         setupEvents();
         setValues();
@@ -29,10 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
     void setValues(){
 
+        act.appNametxt.setText("배달의민족");
+
         fillStores();
 
         myAdapter = new StoreAdapter(MainActivity.this, myStoreList);
-        storeListView.setAdapter(myAdapter);
+        //storeListView.setAdapter(myAdapter);
+        act.storeListView.setAdapter(myAdapter);
     }
 
 //     ArrayLiset 안에 가게들을 채우는 메쏘드, 연습용으로 임시로 작성.
